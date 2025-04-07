0eec4577e07e6a15e59cda9a93fa57830fb44cfe
AngelD3Jesus <angel_3012230140@utd.edu.mx>
2025-04-07 11:23:07 -0600

    fix: usar process.env.PORT para Railway

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
