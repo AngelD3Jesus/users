6d3de384cfcbbe6d50a192cb7025b843739da17a
AngelD3Jesus <angel_3012230140@utd.edu.mx>
2025-04-10 07:29:32 -0600

    cambios de Rabbit en users

--- src/services/rabbitServiceEvent.js
+++ src/services/rabbitServiceEvent.js
@@ -3,24 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const RABBITMQ_URL = process.env.RABBIT_HOST;
 const RABBITMQ_EXCHANGE = "user_event";
 const RABBITMQ_ROUTING_KEY = "user.created";
 
 export async function userCreatedEvent(user) {
   try {
-    const connection = await amqp.connect({
-      protocol: "amqp",
-      hostname: process.env.RABBIT_HOST,
-      port: 5672,
-      username: process.env.RABBIT_USER,
-      password: process.env.RABBIT_PASS,
-    });
+    const connection = await amqp.connect(process.env.RABBITMQ_HOST);
     const channel = await connection.createChannel();
 
-    const exchange = "user_event";
+    const exchange = RABBITMQ_EXCHANGE;
     const queue = "user_created_queue";
-    const routingKey = "user.created";
+    const routingKey = RABBITMQ_ROUTING_KEY;
 
     await channel.assertExchange(exchange, "topic", { durable: true });
     await channel.assertQueue(queue, { durable: true });
@@ -28,14 +21,10 @@ export async function userCreatedEvent(user) {
 
     // Publicar el evento
     const message = JSON.stringify(user);
-    channel.publish(
-      RABBITMQ_EXCHANGE,
-      RABBITMQ_ROUTING_KEY,
-      Buffer.from(message)
-    );
+    channel.publish(exchange, routingKey, Buffer.from(message));
 
     console.log(
-      `[x] exchange "${RABBITMQ_EXCHANGE}", routing key "${RABBITMQ_ROUTING_KEY}": ${message}`
+      `[x] exchange "${exchange}", routing key "${routingKey}": ${message}`
     );
 
     setTimeout(() => {
@@ -43,6 +32,6 @@ export async function userCreatedEvent(user) {
     }, 500);
   } catch (error) {
     console.error("Error al conectar o publicar en RabbitMQ:", error.message);
-    // Continuar sin interrumpir el flujo
+    // Continuar sin interrumpir el flujo principal de la app
   }
 }
